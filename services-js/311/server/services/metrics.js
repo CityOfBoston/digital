092e463e65b37fa29c3906c2a232952be985074c
Fin Hopkins <phopkins@gmail.com>
2017-03-13 17:04:27 -0400

    Merge pull request #97 from CityOfBoston/patch/fix-newrelic-transaction
    Fixes newrelic transactions

--- services-js/311/server/services/metrics.js
+++ services-js/311/server/services/metrics.js
@@ -3,7 +3,7 @@
 
 import newrelic from 'newrelic';
 
-export const measure = (key: string, func: Function) => (...args: any) =>
-  new Promise((resolve) => {
-    Promise.resolve(func(...args)).then(newrelic.createTracer(key, resolve));
-  });
+export const measure = (name: string, group: string, func: Function) => newrelic.createBackgroundTransaction(name, group, (...args) => new Promise((resolve) => {
+  Promise.resolve(func(...args)).then(newrelic.createTracer('fetch', resolve));
+  newrelic.endTransaction();
+}));
