6bc68ab05b268f3664e8ece2376e7acba6027250
Fin Hopkins <phopkins@gmail.com>
2017-03-15 10:54:26 -0400

    Merge pull request #112 from CityOfBoston/compress-next
    Uses express-compression middleware to gzip JS

--- services-js/311/server/next-handlers.js
+++ services-js/311/server/next-handlers.js
@@ -1,5 +1,7 @@
 // @flow weak
 
+import compression from 'compression';
+
 declare class HapiResponse {
   statusCode: number,
   result: any,
@@ -39,9 +41,15 @@ const nextHandler = (app, page, staticQuery) => async ({ method, server, raw: { 
 };
 
 const nextDefaultHandler = (app) => {
+  const compressionMiddleware = compression();
   const handler = app.getRequestHandler();
-  return async ({ raw, url }, hapiReply) => {
-    await handler(raw.req, raw.res, url);
+
+  return async ({ raw: { req, res }, url }, hapiReply) => {
+    // Because Next.js write to the raw response we don't get Hapi's built-in
+    // gzipping. So, we run an express middleware that monkeypatches the raw
+    // response to gzip its output.
+    await new Promise((resolve) => { compressionMiddleware(req, res, resolve); });
+    await handler(req, res, url);
     hapiReply.close(false);
   };
 };
