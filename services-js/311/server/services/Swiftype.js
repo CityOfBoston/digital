ec7f243d00d15def44b8f0b27fcd14c372cf8bef
Fin Hopkins <phopkins@gmail.com>
2017-03-29 13:00:50 -0400

    Merge pull request #180 from CityOfBoston/fewer-recent-requests
    Reduces recent reqeust search to 50

--- services-js/311/server/services/Swiftype.js
+++ services-js/311/server/services/Swiftype.js
@@ -120,7 +120,7 @@ export default class Swiftype {
         engine: this.engine,
         documentType: 'cases',
         q: query || '',
-        per_page: 100,
+        per_page: 50,
         page,
         filters,
         sort_field: { cases: 'updated_datetime' },
