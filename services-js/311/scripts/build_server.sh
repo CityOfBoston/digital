876409a7caa11c38b37ca110a5e22dc61c41b1bd
Fin Hopkins <phopkins@gmail.com>
2017-04-12 09:23:16 -0400

    Merge pull request #243 from CityOfBoston/app-header
    Adds template loading back into the build script

--- services-js/311/scripts/build_server.sh
+++ services-js/311/scripts/build_server.sh
@@ -7,8 +7,8 @@ NODE_ENV=production
 rm -rf build
 rm -rf .next
 
+yarn fetch-templates
+
 yarn babel -- --ignore __mocks__ --ignore *.test.js -d ./build/server server
 yarn babel -- --ignore __mocks__ --ignore *.test.js -d ./build/data data
 yarn next build
-# Temporarily disabled until the new template is live in prod
-# yarn fetch-templates
