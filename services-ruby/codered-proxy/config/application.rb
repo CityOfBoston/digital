8bb79b92a1c10072c0e3ea88d99edb2bbfd2cf98
Matthew Crist <matthew.crist@boston.gov>
2017-05-22 09:11:27 -0400

    Move code red to lib directory

--- services-ruby/codered-proxy/config/application.rb
+++ services-ruby/codered-proxy/config/application.rb
@@ -14,6 +14,5 @@ module CobOemalerts
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W( lib/ )
   end
 end
