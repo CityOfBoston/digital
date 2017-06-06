7d6e24ffc70e0a82e0a7b1022a4f8780033e765b
Matthew Crist <matthew.crist@boston.gov>
2017-06-06 12:29:16 -0400

    Update load order of Dotenv

--- services-ruby/contactform/config/application.rb
+++ services-ruby/contactform/config/application.rb
@@ -1,10 +1,12 @@
 require_relative 'boot'
-
 require 'rails/all'
+require 'dotenv/load'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+Dotenv.load ".env.#{Rails.env}", '.env'
 
 module CobContact
   class Application < Rails::Application
