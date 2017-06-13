0976d49ce639b17c1ed1f8621654ca531a8103ac
Fin Hopkins <phopkins@gmail.com>
2017-06-13 09:49:33 -0400

    Merge pull request #499 from CityOfBoston/img-svg-size
    img, svg size

--- services-js/311/lib/make-css.js
+++ services-js/311/lib/make-css.js
@@ -37,6 +37,12 @@ export default function makeCss(css: ?string) {
         font-family: Lora, serif;
       }
 
+      img, svg {
+        max-width: 100%;
+        height: auto;
+        width: auto;
+      }
+
       #nprogress{pointer-events:none;}
       #nprogress .bar{background:#fcb61a;position:fixed;z-index:1031;top:0;left:0;width:100%;height:2px;}
       #nprogress .peg{display:block;position:absolute;right:0px;width:100px;height:100%;box-shadow:0 0 10px #fcb61a,0 0 5px #fcb61a;opacity:1.0;-webkit-transform:rotate(3deg) translate(0px,-4px);-ms-transform:rotate(3deg) translate(0px,-4px);transform:rotate(3deg) translate(0px,-4px);}
