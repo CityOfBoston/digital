26a2f9c2625f3e5085a0beaf4a1b32af8c920e03
Matthew Crist <matthew.crist@boston.gov>
2017-06-14 11:43:24 -0400

    Only track emails going to allowed domains

--- services-ruby/contactform/app/mailers/contact_mailer.rb
+++ services-ruby/contactform/app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ApplicationMailer
     mail(
       to: email.to_address,
       from: email.from_address,
-      reply_to: get_reply_to(email.token),
+      reply_to: get_reply_to(email),
       subject: email.subject,
       body: email.message
     )
@@ -11,7 +11,14 @@ class ContactMailer < ApplicationMailer
 
   private
 
-  def get_reply_to(token)
-    return "#{token}@#{ENV['EMAIL_HOST']}"
+  def get_reply_to(email)
+    domain = Mail::Address.new(email.to_address).domain
+    allowed_hosts = ENV['ALLOWED_EMAIL_HOSTS'].split(/\s*,\s*/)
+
+    if allowed_hosts.include?(domain)
+      return "#{email.token}@#{ENV['EMAIL_HOST']}"
+    else
+      return email.to_address
+    end
   end
 end
