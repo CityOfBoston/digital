ff13028430b15be4df28ee42178f1d9af843f3aa
Matthew Crist <matthew.crist@boston.gov>
2017-06-20 08:36:20 -0400

    Remove sending email to constituent, and just include in reply

--- services-ruby/contactform/app/lib/email_processor.rb
+++ services-ruby/contactform/app/lib/email_processor.rb
@@ -4,31 +4,21 @@ class EmailProcessor
   end
 
   def process
-    to_address = get_emails(@email.to)
+    token = get_emails(@email.to)
 
-    unless to_address.blank?
-      email_to_send = {
-        :from_address => @email.from[:full],
-        :to_address => get_emails(@email.to),
-        :cc => get_emails(@email.cc),
-        :bcc => get_emails(@email.bcc),
-        :subject => @email.subject,
-        :body => @email.body,
-        :token => get_token(@email.to),
-        :attachments => get_attachments(@email)
-      }
+    unless token.nil?
+      email = Email.find_by(token: token)
 
-      if is_allowed_host?
-        ConstituentMailer.constituent_email(email_to_send).deliver_later
-      else
-        puts "Host not allowed"
+      unless email.nil?
+        email.replied = Time.now
+        email.save
       end
     end
   end
 
   private
 
-  def get_token(addresses)
+  def get_email_token(addresses)
     token = nil
 
     addresses.each do |address|
@@ -38,32 +28,5 @@ class EmailProcessor
     end
 
     token
-  end
-
-  def get_emails(addresses)
-    address_array = []
-
-    addresses.each do |address|
-      if address[:host] == ENV['EMAIL_HOST']
-        email = Email.find_by(token: address[:token])
-
-        unless email.nil?
-          address_array << email.from_address
-
-          if email.replied.nil?
-            email.update(replied: Time.now)
-          end
-        end
-      else
-        address_array << address[:full]
-      end
-    end
-
-    address_array
-  end
-
-  def is_allowed_host?
-    allowed_hosts = ENV['ALLOWED_EMAIL_HOSTS'].split(/\s*,\s*/)
-    return allowed_hosts.include?(@email.from[:host])
   end
 end
--- services-ruby/contactform/test/mailers/constituent_mailer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ConstituentMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
