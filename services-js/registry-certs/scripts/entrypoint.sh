b26716ef3dd7841eade24b45c9ef46460ee57c45
Fin Hopkins <phopkins@gmail.com>
2017-07-17 16:27:09 -0400

    Merge pull request #89 from CityOfBoston/fail-entrypoint
    Fail entrypoint if commands didn't succeed

--- services-js/registry-certs/scripts/entrypoint.sh
+++ services-js/registry-certs/scripts/entrypoint.sh
@@ -3,6 +3,8 @@
 # Entrypoint downloads our configuration from S3. Our task role is
 # authorized to download from this bucket.
 
+set -e
+
 if [ -z "$AWS_S3_CONFIG_URL" ]; then
   echo >&2 'error: missing AWS_S3_CONFIG_URL environment variable'
   exit 1
