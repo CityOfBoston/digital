be115596a0547666cf9b374809846bdb05f0a2ca
Fin Hopkins <phopkins@gmail.com>
2017-07-20 10:49:34 -0400

    Merge pull request #94 from CityOfBoston/cache-for-docker-build
    Adds --cache-from for Docker build speedup

--- services-js/registry-certs/scripts/ecs-deploy.js
+++ services-js/registry-certs/scripts/ecs-deploy.js
@@ -1,3 +1,5 @@
+/* eslint no-console: 0 */
+
 // Builds and deploys to AWS ECS
 //
 // Assumes docker and ecs-cli are installed.
@@ -7,116 +9,16 @@
 
 const { spawn } = require('child_process');
 
-function containerImage(serviceName) {
-  const repositoryPieces = [
-    process.env['AWS_ECS_REGISTRY'],
-    process.env['AWS_ECS_REPOSITORY_NAMESPACE'],
-    serviceName,
-  ];
+// these must match docker-compose.yml
+const APP_SERVER_CONTAINER_NAME = 'app-server';
+const APP_SERVER_CONTAINER_PORT = 3000;
 
-  // We tag the images with the Travis build number, so we can trace that back (and because it’s nicely
-  // incrementing for sort) and the current Git commit so we know what version of the code it is.
-  const containerTag =
-    process.env['TRAVIS_BUILD_NUMBER'] && process.env['TRAVIS_COMMIT']
-      ? `${process.env['TRAVIS_BUILD_NUMBER']}-${process.env['TRAVIS_COMMIT']}`
-      : 'dev';
-
-  return `${repositoryPieces.filter(p => !!p).join('/')}:${containerTag}`;
-}
-
-function buildContainer(image) {
+function spawnPromise(bin, args, opts = {}) {
   return new Promise((resolve, reject) => {
-    const child = spawn('docker', ['build', '-t', image, '.'], {
-      stdio: 'inherit',
-    });
-
-    child.on('error', reject);
-    child.on('close', code => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(code);
-      }
-    });
-  });
-}
-
-function pushContainer(image) {
-  return new Promise((resolve, reject) => {
-    const child = spawn('ecs-cli', ['push', image], {
-      stdio: 'inherit',
-    });
-
-    child.on('error', reject);
-    child.on('close', code => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(code);
-      }
-    });
-  });
-}
-
-function deployService(serviceName, image) {
-  return new Promise((resolve, reject) => {
-    if (!process.env['AWS_ECS_SERVICE_ROLE_ARN']) {
-      throw new Error('AWS_ECS_SERVICE_ROLE_ARN not set');
-    }
-
-    if (!process.env['AWS_ECS_TASK_ROLE_ARN']) {
-      throw new Error('AWS_ECS_TASK_ROLE_ARN not set');
-    }
-
-    if (!process.env['AWS_ECS_CLUSTER']) {
-      throw new Error('AWS_ECS_CLUSTER not set');
-    }
-
-    if (!process.env['AWS_CLOUDWATCH_LOGS_GROUP']) {
-      throw new Error('AWS_CLOUDWATCH_LOGS_GROUP not set');
-    }
-
-    if (!process.env['AWS_EC2_TARGET_GROUP_ARN']) {
-      throw new Error('AWS_EC2_TARGET_GROUP_ARN not set');
-    }
-
-    if (!process.env['AWS_S3_CONFIG_BUCKET']) {
-      throw new Error('AWS_S3_CONFIG_BUCKET not set');
-    }
-
     const child = spawn(
-      'ecs-cli',
-      [
-        'compose',
-        '--project-name',
-        serviceName,
-        '--cluster',
-        process.env.AWS_ECS_CLUSTER,
-        '--task-role-arn',
-        process.env.AWS_ECS_TASK_ROLE_ARN,
-        'service',
-        'up',
-        '--role',
-        process.env.AWS_ECS_SERVICE_ROLE_ARN,
-        '--target-group-arn',
-        process.env.AWS_EC2_TARGET_GROUP_ARN,
-        // these must match docker-compose.yml
-        '--container-name',
-        'app-server',
-        '--container-port',
-        '3000',
-      ],
-      {
-        env: Object.assign({}, process.env, {
-          // Used by docker-compose.yml
-          APP_SERVER_IMAGE: image,
-          AWS_S3_CONFIG_URL: `s3://${process.env
-            .AWS_S3_CONFIG_BUCKET}/${serviceName}`,
-          AWS_CLOUDWATCH_LOGS_GROUP: process.env.AWS_CLOUDWATCH_LOGS_GROUP,
-          AWS_CLOUDWATCH_LOGS_PREFIX: serviceName,
-        }),
-        stdio: 'inherit',
-      }
+      bin,
+      args,
+      Object.assign({}, { stdio: 'inherit' }, opts)
     );
 
     child.on('error', reject);
@@ -130,6 +32,123 @@ function deployService(serviceName, image) {
   });
 }
 
+function getImageRepository(serviceName) {
+  const repositoryPieces = [
+    process.env['AWS_ECS_REGISTRY'],
+    process.env['AWS_ECS_REPOSITORY_NAMESPACE'],
+    serviceName,
+  ];
+
+  return repositoryPieces.filter(p => !!p).join('/');
+}
+
+// We tag the repositorys with the Travis build number, so we can trace that back
+// (and because it’s nicely incrementing for sort) and the current Git commit so
+// we know what version of the code it is.
+function getBuildTag() {
+  return process.env['TRAVIS_BUILD_NUMBER'] && process.env['TRAVIS_COMMIT']
+    ? `${process.env['TRAVIS_BUILD_NUMBER']}-${process.env['TRAVIS_COMMIT']}`
+    : 'dev';
+}
+
+function warmContainerCache(repository) {
+  return spawnPromise('ecs-cli', [
+    'pull',
+    `${repository}:latest`,
+  ]).catch(err => {
+    console.log(
+      `Error warming the container cache for ${repository}:latest. Ignoring.`,
+      err
+    );
+  });
+}
+
+function buildContainer(repository, buildTag) {
+  return spawnPromise(
+    'docker',
+    // we tag both the build and "latest" so we can download this later to
+    // warm the cache
+    [
+      'build',
+      // --cache-from drastically speeds up builds when package.json doesn't
+      // change, since it can re-use all of the npm install results.
+      '--cache-from',
+      `${repository}:latest`,
+      '-t',
+      `${repository}:${buildTag}`,
+      '-t',
+      `${repository}:latest`,
+      '.',
+    ]
+  );
+}
+
+function pushContainer(repository, buildTag) {
+  return Promise.resolve()
+    .then(() => spawnPromise('ecs-cli', ['push', `${repository}:${buildTag}`]))
+    .then(() => spawnPromise('ecs-cli', ['push', `${repository}:latest`]));
+}
+
+function deployService(serviceName, repository, buildTag) {
+  if (!process.env['AWS_ECS_SERVICE_ROLE_ARN']) {
+    throw new Error('AWS_ECS_SERVICE_ROLE_ARN not set');
+  }
+
+  if (!process.env['AWS_ECS_TASK_ROLE_ARN']) {
+    throw new Error('AWS_ECS_TASK_ROLE_ARN not set');
+  }
+
+  if (!process.env['AWS_ECS_CLUSTER']) {
+    throw new Error('AWS_ECS_CLUSTER not set');
+  }
+
+  if (!process.env['AWS_CLOUDWATCH_LOGS_GROUP']) {
+    throw new Error('AWS_CLOUDWATCH_LOGS_GROUP not set');
+  }
+
+  if (!process.env['AWS_EC2_TARGET_GROUP_ARN']) {
+    throw new Error('AWS_EC2_TARGET_GROUP_ARN not set');
+  }
+
+  if (!process.env['AWS_S3_CONFIG_BUCKET']) {
+    throw new Error('AWS_S3_CONFIG_BUCKET not set');
+  }
+
+  return spawnPromise(
+    'ecs-cli',
+    [
+      'compose',
+      '--project-name',
+      serviceName,
+      '--cluster',
+      process.env.AWS_ECS_CLUSTER,
+      '--task-role-arn',
+      process.env.AWS_ECS_TASK_ROLE_ARN,
+      'service',
+      'up',
+      '--role',
+      process.env.AWS_ECS_SERVICE_ROLE_ARN,
+      '--target-group-arn',
+      process.env.AWS_EC2_TARGET_GROUP_ARN,
+      '--container-name',
+      APP_SERVER_CONTAINER_NAME,
+      '--container-port',
+      APP_SERVER_CONTAINER_PORT,
+    ],
+    {
+      env: Object.assign({}, process.env, {
+        // Used by docker-compose.yml
+        APP_SERVER_IMAGE: `${repository}:${buildTag}`,
+        AWS_S3_CONFIG_URL: `s3://${process.env
+          .AWS_S3_CONFIG_BUCKET}/${serviceName}`,
+        AWS_CLOUDWATCH_LOGS_GROUP: process.env.AWS_CLOUDWATCH_LOGS_GROUP,
+        AWS_CLOUDWATCH_LOGS_PREFIX: serviceName,
+      }),
+      stdio: 'inherit',
+    }
+  );
+}
+
 (function() {
   if (!process.env['AWS_ECS_SERVICE_NAME']) {
     throw new Error(`Missing $AWS_ECS_SERVICE_NAME`);
@@ -137,18 +156,18 @@ function deployService(serviceName, image) {
 
   const serviceName = process.env['AWS_ECS_SERVICE_NAME'];
 
-  const image = containerImage(serviceName);
+  const repository = getImageRepository(serviceName);
+  const buildTag = getBuildTag();
 
   Promise.resolve(true)
-    .then(() => buildContainer(image))
-    .then(() => pushContainer(image))
-    .then(() => deployService(serviceName, image))
+    .then(() => warmContainerCache(repository))
+    .then(() => buildContainer(repository, buildTag))
+    .then(() => pushContainer(repository, buildTag))
+    .then(() => deployService(serviceName, repository, buildTag))
     .then(() => {
-      // eslint-disable-next-line no-console
       console.log('Deploy successful.');
     })
     .catch(err => {
-      // eslint-disable-next-line no-console
       console.error('ERROR DEPLOYING CONTAINER', err);
       process.exit(1);
     });
