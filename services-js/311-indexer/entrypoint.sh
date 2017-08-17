e3f19ac705020f80664b5339098fa6ccd3e24fc1
Fin Hopkins <phopkins@gmail.com>
2017-08-17 16:57:52 -0400

    Merge pull request #26 from CityOfBoston/new-deploy-strategy
    Switches to Travis-based deploy

--- services-js/311-indexer/entrypoint.sh
+++ services-js/311-indexer/entrypoint.sh
@@ -5,11 +5,15 @@
 
 set -e
 
+echo "entrypoint.sh: start"
+
 if [ -z "$AWS_S3_CONFIG_URL" ]; then
   echo >&2 'error: missing AWS_S3_CONFIG_URL environment variable'
   exit 1
 fi
 
 aws s3 sync $AWS_S3_CONFIG_URL .
+
+echo "entrypoint.sh: command"
 
 exec "$@"
