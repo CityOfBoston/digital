77cf3dbd62ca2e0e02a02028eba969bbd3b6172e
Fin Hopkins <phopkins@gmail.com>
2017-09-01 15:15:28 -0400

    Merge pull request #42 from CityOfBoston/opbeat-deploy
    Report deploys to Opbeat on startup

--- services-js/311-indexer/server/server.js
+++ services-js/311-indexer/server/server.js
@@ -1,8 +1,10 @@
 // @flow
 /* eslint no-console: 0 */
 
+import os from 'os';
 import Rx from 'rxjs';
 import cleanup from 'node-cleanup';
+import fetch from 'node-fetch';
 
 import Elasticsearch from './services/Elasticsearch';
 import Salesforce from './services/Salesforce';
@@ -28,7 +30,51 @@ type CaseUpdate = {|
   'Incap311__Service_Type_Version_Code__c': string,
 |};
 
+// https://opbeat.com/docs/api/intake/v1/#release-tracking
+async function reportDeployToOpbeat(opbeat) {
+  if (
+    process.env.OPBEAT_APP_ID &&
+    process.env.OPBEAT_ORGANIZATION_ID &&
+    process.env.OPBEAT_SECRET_TOKEN &&
+    process.env.GIT_BRANCH &&
+    process.env.GIT_REVISION
+  ) {
+    try {
+      const res = await fetch(
+        `https://opbeat.com/api/v1/organizations/${process.env
+          .OPBEAT_ORGANIZATION_ID}/apps/${process.env.OPBEAT_APP_ID}/releases/`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: `Bearer ${process.env.OPBEAT_SECRET_TOKEN}`,
+          },
+          body: [
+            `rev=${process.env.GIT_REVISION}`,
+            `branch=${encodeURIComponent(process.env.GIT_BRANCH)}`,
+            `machine=${encodeURIComponent(os.hostname())}`,
+            `status=machine-completed`,
+          ].join('&'),
+        }
+      );
+      console.log(
+        'Reported deploy to Opbeat:',
+        JSON.stringify(await res.json())
+      );
+    } catch (err) {
+      // We swallow the error because we won't interrupt startup because we
+      // couldn't report the release.
+      console.error('Error reporting deploy to Opbeat');
+
+      // bwaaaaaa
+      opbeat.captureError(err);
+    }
+  }
+}
+
 export default async function startServer({ opbeat }: ServerArgs) {
+  reportDeployToOpbeat(opbeat);
+
   let salesforce: ?Salesforce;
   let pipelineSubscription: ?Rx.Subscription;
 
