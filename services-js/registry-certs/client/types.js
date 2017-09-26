bc9b40e938a4a6a98cc09cab9e10cf0e45b58a69
Fin Hopkins <phopkins@gmail.com>
2017-09-26 11:57:10 -0400

    Merge pull request #99 from CityOfBoston/greenkeeper/apollo-codegen-0.16.0
    Update apollo-codegen to the latest version 🚀

--- services-js/registry-certs/client/types.js
+++ services-js/registry-certs/client/types.js
@@ -5,12 +5,15 @@ import type {
   SearchDeathCertificatesQuery,
 } from './queries/graphql-types';
 
-export type DeathCertificate = $ArrayElement<
-  $PropertyType<
-    $PropertyType<FetchDeathCertificatesQuery, 'deathCertificates'>,
-    'certificates'
+export type DeathCertificate = $NonMaybeType<
+  $ArrayElement<
+    $PropertyType<
+      $PropertyType<FetchDeathCertificatesQuery, 'deathCertificates'>,
+      'certificates'
+    >
   >
 >;
+
 export type DeathCertificateSearchResults = $PropertyType<
   $PropertyType<SearchDeathCertificatesQuery, 'deathCertificates'>,
   'search'
