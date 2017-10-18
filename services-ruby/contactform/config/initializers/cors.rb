08d8e6cc72c36e7d51e120b45b277c1bc55103e7
Fin Hopkins <phopkins@gmail.com>
2017-10-18 11:47:22 -0400

    Merge pull request #19 from CityOfBoston/develop
    Prod deploy

--- services-ruby/contactform/config/initializers/cors.rb
+++ services-ruby/contactform/config/initializers/cors.rb
@@ -1,6 +1,14 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  origins = (ENV['ORIGINS'] || '').split(',')
+
+  # Lets us put a matcher in for *.boston.gov so that all our webapps can handle
+  # this, rather than having to enumerate each one.
+  if !ENV['CORS_DOMAIN_ORIGIN'].nil?
+    origins << /^https:\/\/.*\.#{Regexp.escape(ENV['CORS_DOMAIN_ORIGIN'])}$/
+  end
+
   allow do
-    origins (ENV['ORIGINS'] || '').split(',')
+    origins origins
     resource '*',
       headers: :any,
       methods: %i(post)
