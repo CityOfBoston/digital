5546eb41bd52f7417d5e4a2eccb5ecdafb522bab
Fin Hopkins <fin.hopkins@boston.gov>
2018-05-02 09:58:24 -0400

    Adds a Khaos template for JS modules
    Includes test to make sure that the template is instantiable and its
    default tests pass.
    
    Adds the templates’ build directories to our packages so that Yarn will
    install them with workspace links when we test.

---
+++ templates/js-module/src/js-module.test.ts
@@ -0,0 +1,51 @@
+import path from 'path';
+import fs from 'fs';
+import Khaos from 'khaos';
+import tmp from 'tmp';
+import shell from 'shelljs';
+
+let tmpDir;
+
+beforeEach(() => {
+  const buildDir = path.resolve(__dirname, '..', 'build');
+
+  if (!fs.existsSync(buildDir)) {
+    fs.mkdirSync(buildDir);
+  }
+
+  tmpDir = tmp.dirSync({
+    dir: buildDir,
+    unsafeCleanup: true,
+  });
+});
+
+afterEach(() => {
+  tmpDir.removeCallback();
+});
+
+it('generates a template with installable package.json and passing tests', function*() {
+  const khaos = new Khaos('./template');
+
+  const moduleName = path.basename(tmpDir.name);
+  const answers = { name: moduleName, description: 'A test module' };
+
+  const files = yield khaos.read();
+  const schema = yield khaos.parse(files);
+
+  // Guardrail: Ensure that we have values in `answers` for all of the questions
+  // in the template. Otherwise Khaos will try to prompt on the command line.
+  expect(Object.keys(answers)).toEqual(
+    expect.arrayContaining(Object.keys(schema))
+  );
+
+  yield khaos.write(tmpDir.name, files, answers);
+
+  const packageJson = JSON.parse(
+    fs.readFileSync(path.resolve(tmpDir.name, 'package.json'), 'utf-8')
+  );
+  expect(packageJson.name).toEqual(`@cityofboston/${moduleName}`);
+
+  shell.cd(tmpDir.name);
+  expect(shell.exec('yarn install', { silent: true }).code).toEqual(0);
+  expect(shell.exec('yarn run test', { silent: true }).code).toEqual(0);
+});
