82baf36b86048fcfbd337f62edca70bf4d7d0632
John Fleurimond <John.fleurimond@boston.gov>
2018-06-28 16:25:14 -0400

    Zip code Regex completed.

--- services-js/commissions-app/src/pages/commissions/apply.tsx
+++ services-js/commissions-app/src/pages/commissions/apply.tsx
@@ -194,7 +194,6 @@ export default class IndexPage extends React.Component<Props> {
           />
 
           <ul>
-            <input type="checkbox" />
             {commissions.map(commission => this.renderCommission(commission))}
           </ul>
         </div>
