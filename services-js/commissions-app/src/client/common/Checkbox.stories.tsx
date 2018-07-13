9a4a109ad1483c15195ac39fd6bc634187ed8053
John Fleurimond <John.fleurimond@boston.gov>
2018-07-13 11:37:23 -0400

    Checkbox complete, Functionality next

--- services-js/commissions-app/src/client/common/Checkbox.stories.tsx
+++ services-js/commissions-app/src/client/common/Checkbox.stories.tsx
@@ -3,5 +3,11 @@ import { storiesOf } from '@storybook/react';
 import Checkbox from './Checkbox';
 
 storiesOf('Checkbox', module).add('default', () => (
-  <Checkbox name="checkbox" title="" value="checkbox" onChange="" onBlur="" />
+  <Checkbox
+    name="checkbox"
+    title="Community Preservation Committee"
+    value="checkbox"
+    onChange=""
+    onBlur=""
+  />
 ));
--- services-js/commissions-app/src/client/common/Checkbox.tsx
+++ services-js/commissions-app/src/client/common/Checkbox.tsx
@@ -11,19 +11,17 @@ export interface CheckboxProps {
 export default class Checkbox extends React.Component<CheckboxProps> {
   render() {
     return (
-      <div>
-        <label className="cb">
-          <input
-            name={this.props.name}
-            value={this.props.value}
-            type="checkbox"
-            className="cb-f"
-            onChange={this.props.onChange}
-            onBlur={this.props.onBlur}
-          />
-          <span className="cb-l">{this.props.title}</span>
-        </label>
-      </div>
+      <label className="cb">
+        <input
+          name={this.props.name}
+          value={this.props.value}
+          type="checkbox"
+          className="cb-f"
+          onChange={this.props.onChange}
+          onBlur={this.props.onBlur}
+        />
+        <span className="cb-l">{this.props.title}</span>
+      </label>
     );
   }
 }
