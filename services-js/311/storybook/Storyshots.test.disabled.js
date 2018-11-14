05aa1ae7badc0a1129a7db70ef74a2ef0c4fd5d8
Fin Hopkins <fin.hopkins@boston.gov>
2018-11-14 09:14:51 -0500

    Dependency updates to work in Monorepo
    The graphql library is especially sensitive to version mismatches, but
    this updates some other things as well to reduce our package install
    footprint.

--- services-js/311/storybook/Storyshots.test.disabled.js
+++ services-js/311/storybook/Storyshots.test.disabled.js
@@ -2,6 +2,8 @@ import initStoryshots, {
   snapshotWithOptions,
 } from '@storybook/addon-storyshots';
 
+require('babel-plugin-require-context-hook/register')();
+
 function createNodeMock(element) {
   return document.createElement(element.type);
 }
