22e4ebf38a00068a47e86876a55454d7e098a72c
Fin Hopkins <fin.hopkins@boston.gov>
2018-12-07 16:13:25 -0500

    Waits for the React app to start in TestCafe
    Part of our TestCafe flakiness is that the forms are getting filled out
    before React is done starting up, so when it does the form fields get
    wiped out by the empty values.
    
    This makes sure that the isReactRunning function is returning true
    before we exit out of the logIn helper.

--- services-js/access-boston/src/integration/models/LoginFormModel.ts
+++ services-js/access-boston/src/integration/models/LoginFormModel.ts
@@ -1,5 +1,6 @@
 import { Selector } from 'testcafe';
 import PageModel from './PageModel';
+import { isReactRunning } from '../testcafe-helpers';
 
 export default class LoginFormModel extends PageModel {
   root = Selector('form');
@@ -7,10 +8,15 @@ export default class LoginFormModel extends PageModel {
   submitButton = this.root.find('input[type=submit]');
 
   logIn(t: TestController, userId: string): Promise<TestController> {
-    return t
-      .typeText(this.userIdField, userId, {
-        replace: true,
-      })
-      .click(this.submitButton);
+    return (
+      t
+        .typeText(this.userIdField, userId, {
+          replace: true,
+        })
+        .click(this.submitButton)
+        // Make sure the app is going after we’ve submitted
+        .expect(isReactRunning())
+        .ok()
+    );
   }
 }
