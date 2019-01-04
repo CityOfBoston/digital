dd0af681fe5e0ba479f20267e87c626f9808bbb4
Fin Hopkins <fin.hopkins@boston.gov>
2019-01-04 09:27:49 -0500

    Removes the birth certificate search query
    Since we’re changing to just make requests rather than look up
    certificates, we don’t need to query the database this way.

--- services-js/registry-certs/server/graphql/query.ts
+++ services-js/registry-certs/server/graphql/query.ts
@@ -1,16 +1,13 @@
 import { Resolvers, ResolvableWith } from '@cityofboston/graphql-typescript';
 import { DeathCertificates } from './death-certificates';
-import { BirthCertificates } from './birth-certificates';
 import { Context } from '.';
 
 export interface Query extends ResolvableWith<{}> {
   deathCertificates: DeathCertificates;
-  birthCertificates: BirthCertificates;
 }
 
 const queryResolvers: Resolvers<Query, Context> = {
   deathCertificates: () => ({}),
-  birthCertificates: () => ({}),
 };
 
 export const resolvers = {
