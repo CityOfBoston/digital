3fdaa2e938dec98cccd7edcd9da68a3bc1bad47b
Fin Hopkins <fin.hopkins@boston.gov>
2019-01-24 15:54:27 -0500

    Error page for react-fleet
    Makes a Boston-branded error / 404 page for commissions-app and
    registry-certs so that when they’re down tonight we will have a nicer
    message than "500".

---
+++ modules-js/react-fleet/src/pages/ErrorPage.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import ErrorPage from './ErrorPage';
+
+storiesOf('Pages/Error Page', module)
+  .add('404', () => <ErrorPage statusCode={404} />)
+  .add('500', () => <ErrorPage statusCode={500} />);
