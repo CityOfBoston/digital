25836b065d40cf83a09218d52e892528eaee8398
Fin Hopkins <fin.hopkins@boston.gov>
2019-03-11 10:16:04 -0400

    Rewrites "unsubscribe" subject lines
    Prevents Google Groups from eating the message rather than sending it
    along to the distribution list.

--- services-ruby/contactform/app/controllers/emails_controller.rb
+++ services-ruby/contactform/app/controllers/emails_controller.rb
@@ -5,6 +5,13 @@ class EmailsController < ApplicationController
 
   def create
     @email = Email.new(email_params)
+
+    # We need to prevent Google Groups from thinking that the contact form is trying to
+    # unsubscribe from whatever distribution list is the recipient of the email.
+    if @email.subject =~ /unsubscribe/i || @email.subject =~ /remove me/i
+      @email.subject = "Subscription question"
+    end
+
     @email.ip = request.remote_ip
     @email.sent = DateTime.now.utc
 
