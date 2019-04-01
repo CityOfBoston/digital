1730a695cafade02e435b1d742a48b24a0a5f2e4
Jessica Marcus <jessica.marcus@boston.gov>
2019-04-01 16:29:59 -0400

    Adds ability to use element instead of string for label of component

--- modules-js/react-fleet/src/form-elements/inputs/TextInput.tsx
+++ modules-js/react-fleet/src/form-elements/inputs/TextInput.tsx
@@ -2,18 +2,32 @@ import React from 'react';
 import hash from 'string-hash';
 
 /**
- * Stateless component for rendering a text-like form element. (Type of "text",
- * "tel", "password", &c.)
+ * Stateless component for rendering a text-like form element. (Type of “text”,
+ * “tel”, “password”, &c.)
  *
- * Automatically generates an id for the element if one is not provided. Also
- * automatically adds the "txt-f" class to the element, and the "txt-f--err"
- * class if there's an error.
+ * If necessary, an element can be passed in as the “label” property instead
+ * of text. Its children should only contain plain text. The most common case
+ * would be several sibling <span>s:
  *
- * If hideLabel is specified, the label text will be added as an aria-label
- * attribute on the input element.
+ * label={
+ *  <>
+ *    <span style={{ whiteSpace: 'nowrap' }}>Label will be very long</span>
+ *    <wbr />
+ *    <span style={{ whiteSpace: 'nowrap' }}>
+ *      and we want some control over how the browser will break the text
+ *    </span>
+ *  </>
+ *  }
+ *
+ * Automatically generates an id for the element if one is not provided. Will
+ * also add the “txt-f” class to the element, and the “txt-f--err” class if
+ * there’s an error.
+ *
+ * If “hideLabel” is specified, the label text will be added as an aria-label
+ * attribute on the <input> element.
  */
 interface Props {
-  label: string;
+  label: string | React.ReactNode;
   type?: string;
   small?: boolean;
   hideLabel?: boolean;
@@ -50,24 +64,56 @@ export default function TextInput(props: Props): JSX.Element {
     }`,
   };
 
+  const labelText = (label): React.ReactChild => {
+    if (typeof label === 'string') {
+      return (
+        <span style={{ marginRight: '0.5em', whiteSpace: 'nowrap' }}>
+          {label}
+        </span>
+      );
+    } else {
+      return label;
+    }
+  };
+
+  // If a React node is passed in for the input’s label, but “hideLabel” is
+  // true, the inner text is extracted from each child node so we have plain
+  // text for the “aria-label” attribute. Self-closing tags (e.g <wbr /> or
+  // <br />) are ignored. Child nodes may only contain plain text.
+  const extractAriaLabelText = (label): string => {
+    if (typeof label === 'string') {
+      return label;
+    } else {
+      return label.props.children
+        .reduce((acc, node) => {
+          if (node.props.children) {
+            return acc + node.props.children + ' ';
+          } else {
+            return acc;
+          }
+        }, '')
+        .trim();
+    }
+  };
+
   return (
     <div className={props.className} style={props.style}>
       <label htmlFor={id} className={classNames.label}>
-        {props.hideLabel ? (
-          <>&nbsp;</>
-        ) : (
-          <span style={{ marginRight: '0.5em', whiteSpace: 'nowrap' }}>
-            {props.label}
+        {props.hideLabel ? <>&nbsp;</> : labelText(props.label)}
+
+        {/* Because “required” attribute is set on <input> element, */}
+        {/* hide this from screen readers to cut down on repetition. */}
+        {props.required && (
+          <span className="t--req" aria-hidden="true">
+            Required
           </span>
         )}
-
-        {props.required && <span className="t--req">Required</span>}
       </label>
 
       <input
         className={classNames.input}
         id={id}
-        aria-label={props.hideLabel ? props.label : ''}
+        aria-label={props.hideLabel ? extractAriaLabelText(props.label) : ''}
         type={props.type || 'text'}
         name={props.name}
         placeholder={props.placeholder}
@@ -85,7 +131,7 @@ export default function TextInput(props: Props): JSX.Element {
       />
 
       <div className="t--subinfo t--err m-b100">
-        {/* The &nbsp; is to keep space for the error so the form doesn't jump when one is added. */}
+        {/* The &nbsp; is to keep space for the error so the form doesn’t jump when one is added. */}
         {props.error && typeof props.error === 'string' ? (
           props.error
         ) : (
