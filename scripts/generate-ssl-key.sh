1c4019c1526027dcd3073e0e8c221441abde7822
Fin Hopkins <fin.hopkins@boston.gov>
2019-04-23 12:53:15 -0400

    Makes key generation password longer
    Fixes "You must type in 4 to 1023 characters" error when generating a
    password.

--- scripts/generate-ssl-key.sh
+++ scripts/generate-ssl-key.sh
@@ -6,8 +6,8 @@ FILENAME_BASE=${2:-server}
 
 cd $1
 
-openssl genrsa -des3 -passout pass:x -out "${FILENAME_BASE}.pass.key" 2048
-openssl rsa -passin pass:x -in "${FILENAME_BASE}.pass.key" -out "${FILENAME_BASE}.key"
+openssl genrsa -des3 -passout pass:swordfish -out "${FILENAME_BASE}.pass.key" 2048
+openssl rsa -passin pass:swordfish -in "${FILENAME_BASE}.pass.key" -out "${FILENAME_BASE}.key"
 rm "${FILENAME_BASE}.pass.key"
 openssl req -new -key "${FILENAME_BASE}.key" -out "${FILENAME_BASE}.csr" \
   -subj "/C=US/ST=Massachusetts/L=Boston/O=Department of Innovation and Technology/OU=Digital/CN=${WORKSPACE:-default}"
