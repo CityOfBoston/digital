93ba06ba9b68b799ddebf97772aad6c08fb93974
Phillip Kelly <phillipbentonkelly@gmail.com>
2019-10-21 17:43:07 -0400

    Re-added endpoint URL

--- services-js/access-boston/src/client/group-management/data-fetching/fetchGraphql.ts
+++ services-js/access-boston/src/client/group-management/data-fetching/fetchGraphql.ts
@@ -1,7 +1,9 @@
 import fetch from 'node-fetch';
 
 export async function fetchGraphql(query: string, variables: any) {
-  const groupManagementApiUrl = process.env.GROUP_MANAGEMENT_API_URL;
+  const groupManagementApiUrl =
+    process.env.GROUP_MANAGEMENT_API_URL ||
+    'https://group-mgmt.digital-staging.boston.gov/graphql';
   // eslint-disable-next-line no-console
   // console.log('fetchGraphql > variables: ', variables);
   // console.log('fetchGraphql > query: ', query);
