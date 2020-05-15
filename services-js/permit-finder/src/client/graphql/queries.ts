dafa94b373a4788004795cc368d904983b52e8a0
Phillip Kelly <phillipbentonkelly@gmail.com>
2020-05-15 15:41:14 -0400

    Permit-Finder: Mobile input issue and Iterators VPAT tickets

--- services-js/permit-finder/src/client/graphql/queries.ts
+++ services-js/permit-finder/src/client/graphql/queries.ts
@@ -1,5 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
+// @generated
 // This file was automatically generated and should not be edited.
 
 // ====================================================
@@ -39,6 +40,7 @@ export interface LoadPermitVariables {
 
 /* tslint:disable */
 /* eslint-disable */
+// @generated
 // This file was automatically generated and should not be edited.
 
 //==============================================================
