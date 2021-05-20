525fc82382d93daf00524336645335e2b4921133
Phillip Kelly <phillipbentonkelly@gmail.com>
2021-05-20 16:18:50 -0400

    AWS Deploy: Silence AWS Dockerhub login

--- deploy/dockerHubRateLimit.sh
+++ deploy/dockerHubRateLimit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e
+# set -e
 
-export DOCKERHUBCREDS="${DOCKERHUB_USERNAME}:${DOCKERHUB_PASSWORD}"
-AUTHSTRIN="Authorization: Bearer "
-TOKEN=$(curl --user $DOCKERHUBCREDS "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | grep -Po '"token": *\K"[^"]*"' | sed -e 's/^"//' -e 's/"$//')
-curl -v -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest 2>&1 | grep RateLimit
+# export DOCKERHUBCREDS="${DOCKERHUB_USERNAME}:${DOCKERHUB_PASSWORD}"
+# AUTHSTRIN="Authorization: Bearer "
+# TOKEN=$(curl --user $DOCKERHUBCREDS "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | grep -Po '"token": *\K"[^"]*"' | sed -e 's/^"//' -e 's/"$//')
+# curl -v -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest 2>&1 | grep RateLimit
