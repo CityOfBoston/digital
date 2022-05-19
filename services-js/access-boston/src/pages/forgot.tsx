0bafbc631d41d59bc9508a9cd133c252f0999e05
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2022-05-19 15:12:46 -0400

    Revert "Access-Boston: HCM Cutover, force success view"

--- services-js/access-boston/src/pages/forgot.tsx
+++ services-js/access-boston/src/pages/forgot.tsx
@@ -331,10 +331,6 @@ function passwordErrorToFormErrors(error: string | null) {
     typeof error === 'string' &&
     error.includes('sailpoint.connector.ConnectorException')
   ) {
-    this.setState({
-      showSuccessMessage: true,
-      showSubmittingModal: false,
-    });
     return {};
   }
 
