a098a172b9a6ff7fa71f8521ab1c71f862f153a6
Phillip Kelly <phillipbentonkelly@gmail.com>
2024-11-01 00:03:02 -0400

    DIG-5226: Basic App Setup - Storybook stories Added

---
+++ services-js/access-boston/src/client/preferred-chosen-name/views/Index.stories.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import PageWrapper from '../../PageWrapper';
+
+import { AppTitle } from '../state/app';
+
+import WelcomeView, { EnterNameView, ApprovalView, successView } from './views';
+import SuccessView from './successView';
+
+const viewAccountObj = {
+  cobAgency: 'CH',
+  firstName: 'Felipe',
+  lastName: 'Rivera',
+  email: 'felipe.rivera@boston.gov',
+};
+
+storiesOf('Preferred-Name', module)
+  .add('Welcome', () => (
+    <PageWrapper classString={'b-c'}>
+      <WelcomeView
+        handleProceed={() => {}}
+        appTitle={AppTitle}
+        account={viewAccountObj}
+      />
+    </PageWrapper>
+  ))
+  .add('Enter Names', () => (
+    <PageWrapper classString={'b-c'}>
+      <EnterNameView
+        handleProceed={() => {}}
+        handleStepBack={() => {}}
+        appTitle={AppTitle}
+        account={viewAccountObj}
+      />
+    </PageWrapper>
+  ))
+  .add('Approval', () => (
+    <PageWrapper classString={'b-c'}>
+      <ApprovalView
+        handleProceed={() => {}}
+        handleStepBack={() => {}}
+        appTitle={AppTitle}
+        account={viewAccountObj}
+      />
+    </PageWrapper>
+  ))
+  .add('Success', () => (
+    <PageWrapper classString={'b-c'}>
+      <SuccessView
+        handleQuit={() => {}}
+        appTitle={AppTitle}
+        account={viewAccountObj}
+      />
+    </PageWrapper>
+  ));
